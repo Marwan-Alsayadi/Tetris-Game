f51944816cda4264f565def291514419cd3052cc
Marwan Alsayadi <marwantec5@gmail.com>
2025-05-26 13:41:04 +0300

    Configure Vite base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-const repoName = "Tetris-Game";
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, // Replace with your repository name
+  base: "./", // Replace with your repository name
   build: {
     outDir: "dist",
   },
